d15bb93b3dc856970f3506bace711511e1425e00
KainTenebris <43878231+KainTenebris@users.noreply.github.com>
2019-02-05 14:37:51 +0000

    Update BossScreen.java

--- core/src/com/rear_admirals/york_pirates/screen/BossScreen.java
+++ core/src/com/rear_admirals/york_pirates/screen/BossScreen.java
@@ -7,17 +7,12 @@ import com.rear_admirals.york_pirates.screen.combat.CombatScreen;
 
 import static com.rear_admirals.york_pirates.ShipType.Warship;
 
+//just in case want to change how a boss would appear
+//music/backgrounds etc
 public class BossScreen extends CombatScreen {
 
     //Constructor
     public BossScreen(PirateGame game) {
         super(game, new Ship(Warship, "YSJ Admiral", new College("YSJ")));
     }
-
-    //
-    public void update(float delta) {   }
-
-    //renders the screen
-    public void render(float delta) { super.render(delta); }
-
 }
