1ef7af2b3e7fbf325e26ec445d1eacc94349eef5
KainTenebris <43878231+KainTenebris@users.noreply.github.com>
2019-02-05 13:52:57 +0000

    Commented

--- core/src/com/rear_admirals/york_pirates/screen/MainMenu.java
+++ core/src/com/rear_admirals/york_pirates/screen/MainMenu.java
@@ -20,34 +20,36 @@ import static com.rear_admirals.york_pirates.College.*;
 
 public class MainMenu extends BaseScreen {
     private Stage stage;
-
     private float screen_width;
     private float screen_height;
-
+    
+    //Constructor
     public MainMenu(final PirateGame pirateGame){
         super(pirateGame);
+        
+        this.screen_width = stage.getWidth();
+        this.screen_height = stage.getHeight();
 
         Gdx.graphics.setTitle("York Pirates!");
 
-        // Layout Properties
-        Container<Table> tableContainer = new Container<Table>();
-        tableContainer.setFillParent(true);
-        tableContainer.setPosition(0,0);
-        tableContainer.align(Align.center);
-        Table table = new Table();
-        stage = new Stage(new FitViewport(1920,1080));
-
-        screen_width = stage.getWidth();
-        screen_height = stage.getHeight();
-
+        
+        
+        //Labels
+        //create labels
         Label title = new Label("Rear Admirals", pirateGame.getSkin(), "title");
+        
+        //set alignment of labels
         title.setAlignment(Align.center);
-
+        
+        
+        
+        //TextButtons
+        //create TextButtons
         TextButton sailing_mode = new TextButton("Start Game", pirateGame.getSkin()); // Starts sailing mode.
         TextButton quickplay = new TextButton("Quick Play", pirateGame.getSkin());
         TextButton quit = new TextButton("Quit", pirateGame.getSkin());
-
-        // Allows button to be clickable, and sets process for when clicked.
+        
+        //adds listeners to buttons
         quickplay.addListener(new ClickListener(){
             @Override
             public void clicked(InputEvent event, float x, float y) {
@@ -58,7 +60,6 @@ public class MainMenu extends BaseScreen {
                 dispose();
             }
         });
-
         sailing_mode.addListener(new ClickListener(){
             @Override
             public void clicked(InputEvent event, float x, float y){
@@ -66,17 +67,24 @@ public class MainMenu extends BaseScreen {
                 dispose();
             }
         });
-
-
         quit.addListener(new ClickListener() {
             @Override
             public void clicked(InputEvent event, float x, float y) {
                Gdx.app.exit();
             }
         });
-
-        tableContainer.setActor(table);
-
+        
+        
+        
+        //Tables
+        //create tables
+        Table table = new Table();
+        
+        tableContainer.setFillParent(true);
+        tableContainer.setPosition(0,0);
+        tableContainer.align(Align.center);
+        
+        //adds to tables
         table.add(title).padBottom(viewwidth/20).width(viewwidth/2);
         table.row(); // Ends the current row
         table.add(sailing_mode).uniform().padBottom(viewheight/40).size(viewwidth/2,viewheight/10);
@@ -84,15 +92,32 @@ public class MainMenu extends BaseScreen {
         table.add(quickplay).uniform().padBottom(viewheight/40).fill();
         table.row();
         table.add(quit).uniform().fill().padBottom(viewheight/40);
+        
+        
+        
+        //Stages
+        Container<Table> tableContainer = new Container<Table>();
+        this.stage = new Stage(new FitViewport(1920,1080));
 
+        tableContainer.setActor(table);
         stage.addActor(tableContainer);
 
         Gdx.input.setInputProcessor(stage);
     }
 
+    //methods do nothing
     @Override
     public void update(float delta) { }
+    @Override
+    public void show() {    }
+    @Override
+    public void hide() {    }
+    @Override
+    public void pause() {   }
+    @Override
+    public void resume() {  }
 
+    //creates the screen
     public void render(float delta){
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
@@ -100,31 +125,15 @@ public class MainMenu extends BaseScreen {
         stage.act();
     }
 
+    //resizes screen
     @Override
     public void resize(int width, int height) {
         stage.getViewport().update(width,height);
     }
 
+    //disposes of screen
     @Override
-    public void show() {
-    }
-
-    @Override
-    public void hide() {
-    }
-
-    @Override
-    public void pause() {
-    }
-
-    @Override
-    public void resume() {
-    }
-
-    @Override
-    public void dispose() {
-        stage.dispose();
-    }
+    public void dispose() { stage.dispose(); }
 }
 
 
