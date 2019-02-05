95beba25ddf043d6be7d84d4c2fc02de2b029c91
KainTenebris <43878231+KainTenebris@users.noreply.github.com>
2019-02-05 16:50:42 +0000

    Commented

--- core/src/com/rear_admirals/york_pirates/screen/combat/CombatShip.java
+++ core/src/com/rear_admirals/york_pirates/screen/combat/CombatShip.java
@@ -5,16 +5,17 @@ import com.badlogic.gdx.graphics.g2d.Batch;
 import com.rear_admirals.york_pirates.base.BaseActor;
 
 public class CombatShip extends BaseActor {
-
     private float ship_size;
     private Texture texture;
 
+    //Constructor
     public CombatShip(String shipFile, float ship_size){
         this.texture = new Texture(shipFile);
         this.ship_size = ship_size;
         this.setBounds(getX(), getY(), ship_size,ship_size);
     }
 
+    //draws the ship
     @Override
     public void draw(Batch batch, float alpha){
         batch.setColor(1,1,1, alpha);
