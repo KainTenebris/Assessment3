ed77026db77922d5bdd7e6ef68f70e65d302b2d8
KainTenebris <43878231+KainTenebris@users.noreply.github.com>
2019-02-05 12:49:23 +0000

    Commented

--- core/src/com/rear_admirals/york_pirates/Player.java
+++ core/src/com/rear_admirals/york_pirates/Player.java
@@ -9,58 +9,53 @@ import java.util.List;
 import static com.rear_admirals.york_pirates.ShipType.*;
 
 public class Player {
-    private Ship playerShip;
-    private int gold;
-    private int points;
-    public static List<Attack> attacks = new ArrayList<Attack>();
+	private Ship playerShip;
+	private int gold;
+	private int points;
+	public static List<Attack> attacks = new ArrayList<Attack>();
 
-    public Player() {
-	    this.playerShip = new Ship(Player, "Your Ship", PirateGame.colleges.get("Derwent"));
-        this.gold = 50;
-        this.points = 0;
+	//Constructor without a given ship
+	public Player() {
+		this.playerShip = new Ship(Player, "Your Ship", PirateGame.colleges.get("Derwent"));
+		this.gold = 50;
+		this.points = 0;
 
-        attacks.add(Ram.attackRam);
-        attacks.add(GrapeShot.attackSwivel);
-        attacks.add(Attack.attackBoard);
-    }
+		attacks.add(Ram.attackRam);
+		attacks.add(GrapeShot.attackSwivel);
+		attacks.add(Attack.attackBoard);
+	}
 
-    public Player(Ship ship) {
-        this.playerShip = ship;
-        this.gold = 0;
-        this.points = 0;
+	//Constructor with a given ship
+	public Player(Ship ship) {
+		this.playerShip = ship;
+		this.gold = 0;
+		this.points = 0;
 
-        attacks.add(Ram.attackRam);
-        attacks.add(Attack.attackSwivel);
-        attacks.add(Attack.attackBoard);
-    }
+		attacks.add(Ram.attackRam);
+		attacks.add(Attack.attackSwivel);
+		attacks.add(Attack.attackBoard);
+    	}
 
-    public Ship getPlayerShip() { return this.playerShip; }
-
-    public void setPlayerShip(Ship ship) {
-        this.playerShip = ship;
-    }
-
-    public int getPoints() { return points; }
-
+	//Getters
+	public Ship getPlayerShip() { return this.playerShip; }
+	public int getPoints() { return points; }
 	public int getGold() { return gold; }
 
-    public void setPoints(int points) {
-        this.points = points;
-    }
-
-    public void setGold(int gold) { this.gold = gold; }
-
+	//Setters
+	public void setPlayerShip(Ship ship) { this.playerShip = ship; }
+	public void setPoints(int points) { this.points = points; }
+	public void setGold(int gold) { this.gold = gold; }
+	public void addPoints(int amount) { points += amount; }
+	public void addGold(int amount) { gold += amount; }
+	
+	//allows player to pay gold
 	public boolean payGold(int amount){
-        if (amount > gold){
-            return false;
-        }
-        else{
-            addGold(-amount);
-            return true;
+		if (amount > gold){
+			return false;
+		}
+		else{
+			addGold(-amount);
+			return true;
         }
     }
-
-    public void addPoints(int amount) { points += amount; }
-
-    public void addGold(int amount) { gold += amount; }
 }
